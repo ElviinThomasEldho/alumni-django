a8ca7f8e436ae7132d112360e21659c3f9940fd1
Gautham C Sudheer <csgautham091003@gmail.com>
2024-02-20 20:43:11 +0530

    Chat Layout

--- alumni/room/urls.py
+++ alumni/room/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='rooms'),
+    path('create/', views.create_room, name='create_room'),
     path('<slug:slug>/', views.room, name='room'),
 ]
--- alumni/room/views.py
+++ alumni/room/views.py
@@ -1,17 +1,41 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import Http404, HttpResponseBadRequest
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
 
 from .models import Room, Message
+from .forms import RoomForm
 
-@login_required
 def rooms(request):
     rooms = Room.objects.all()
-
     return render(request, 'room/rooms.html', {'rooms': rooms})
 
-@login_required
-def room(request, slug):
-    room = Room.objects.get(slug=slug)
-    messages = Message.objects.filter(room=room)[0:25]
+def room(request, slug=None):
+    rooms = Room.objects.all()
+    if slug is None:
+        return HttpResponseBadRequest("Room slug is required")
 
-    return render(request, 'room/room.html', {'room': room, 'messages': messages})
+    try:
+        room = Room.objects.get(slug=slug)
+    except Room.DoesNotExist:
+        raise Http404("Room does not exist")
+
+    messages = Message.objects.filter(room=room)[:25]
+    context = {'rooms': rooms, 'room': room, 'messages': messages}
+    return render(request, 'room/room.html', context)
+
+def room_view(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'room.html', context)
+
+def create_room(request):
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.slug = slugify(room.name)  # Generate slug from room name
+            room.save()
+            return redirect('rooms')
+    else:
+        form = RoomForm()
+    return render(request, 'room/create_room.html', {'form': form})
