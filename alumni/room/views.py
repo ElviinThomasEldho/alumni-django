ea6b337d4f54853db4e7254491f919f26395a7f5
Gautham C Sudheer <csgautham091003@gmail.com>
2024-02-23 11:12:17 +0530

    Chats Account Update

--- alumni/room/views.py
+++ alumni/room/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .models import Room, Message
+from main.models import Account
+
 from .forms import RoomForm
 
 @login_required
@@ -15,7 +17,9 @@ def rooms(request):
     # Exclude 'personal_' rooms from the list displayed to users
     rooms = Room.objects.exclude(name__startswith='personal_').all()
     users = User.objects.exclude(username=request.user.username)
-    return render(request, 'room/rooms.html', {'rooms': rooms, 'users': users})
+    accounts = Account.objects.exclude(user=request.user).all()
+    account = Account.objects.get(user=request.user)
+    return render(request, 'room/rooms.html', {'rooms': rooms, 'users': users,'accounts': accounts, 'account': account})
 
 @login_required
 def room(request, slug=None):
@@ -31,8 +35,23 @@ def room(request, slug=None):
     rooms = Room.objects.exclude(name__startswith='personal_').all()  # Retrieve all rooms excluding 'personal_' rooms
     messages = Message.objects.filter(room=room)[:25]
     users = User.objects.exclude(username=request.user.username)
-    context = {'room': room, 'messages': messages, 'users': users, 'rooms': rooms}
-    return render(request, 'room/room.html', context)
+    accounts = Account.objects.exclude(user=request.user).all()
+    account = Account.objects.get(user=request.user)
+
+    if room.type == 'direct':
+        members = list(room.users.all())
+
+        for member in members:
+            if (member != request.user):
+                target = member
+                print(target)
+        target = Account.objects.get(user=target)
+
+        context = {'room': room, 'messages': messages, 'users': users, 'rooms': rooms, 'accounts': accounts, 'account': account, 'target': target}
+        return render(request, 'room/room.html', context)
+    else:
+        context = {'room': room, 'messages': messages, 'users': users, 'rooms': rooms, 'accounts': accounts, 'account': account, 'target': room.name}
+        return render(request, 'room/room.html', context)
 
 
 
@@ -42,6 +61,7 @@ def create_room(request):
         if form.is_valid():
             room = form.save(commit=False)
             room.slug = slugify(room.name)  # Generate slug from room name
+            room.type = 'group'
             room.save()
             return redirect('rooms')
     else:
@@ -57,12 +77,16 @@ def create_personal_room(request, username):
     # Generate slugs
     slug_1 = slugify(room_name_1)
     slug_2 = slugify(room_name_2)
-
+    target = User.objects.get(username=username)
     # Check if a room with either name exists
     try:
         room = Room.objects.get(slug__in=[slug_1, slug_2])
     except Room.DoesNotExist:
         # Create a new room if no existing room is found
         room = Room.objects.create(name=room_name_1, slug=slug_1)
+        room.users.add(request.user)
+        room.users.add(target)
+        room.type = 'direct'
+        room.save()
 
     return redirect('room', slug=room.slug)
