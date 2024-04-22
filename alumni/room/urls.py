d03d39ed7d39b7752d90f8c8dda84f4ca4051197
Gautham C Sudheer <csgautham091003@gmail.com>
2024-04-22 08:38:48 +0530

    minor changes

--- alumni/room/urls.py
+++ alumni/room/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.rooms, name='rooms'),
     path('create/', views.create_room, name='create_room'),
+    path('newchat/', views.newchat, name='newchat'),
     path('<slug:slug>/', views.room, name='room'),
     path('create_personal_room/<str:username>/', views.create_personal_room, name='create_personal_room'),
 ]
--- alumni/room/views.py
+++ alumni/room/views.py
@@ -14,12 +14,34 @@ from .forms import RoomForm
 
 @login_required
 def rooms(request):
-    # Exclude 'personal_' rooms from the list displayed to users
-    rooms = Room.objects.exclude(name__startswith='personal_').all()
+    # Include 'personal_' rooms in the list displayed to users
+    rooms = Room.objects.all()
+    public_rooms = Room.objects.exclude(name__startswith='personal_').all()
     users = User.objects.exclude(username=request.user.username)
     accounts = Account.objects.exclude(user=request.user).all()
     account = Account.objects.get(user=request.user)
-    return render(request, 'room/rooms.html', {'rooms': rooms, 'users': users,'accounts': accounts, 'account': account})
+    
+    # Create a list to store 'personal_' rooms
+    personal_rooms = []
+
+    for room in rooms:
+        # Check if the room name starts with 'personal_' and includes the current user's username
+        if room.name.startswith('personal_') and request.user.username in room.name:
+            # Extract the other user's username from the room name
+            other_username = room.name.replace('personal_', '').replace(request.user.username, '').strip('_')
+            # Get the other user's account
+            other_user_account = Account.objects.get(user__username=other_username)
+            # Create a dictionary to represent the 'personal_' room
+            personal_room = {
+                'name': f'{other_user_account.firstName} {other_user_account.lastName}',
+                'username': other_username,
+                'profile_image': other_user_account.profilePicture.url,
+                'slug': room.slug
+            }
+            # Add the 'personal_' room to the list
+            personal_rooms.append(personal_room)
+
+    return render(request, 'room/rooms.html', {'username': other_username, 'public_rooms': public_rooms, 'rooms': rooms, 'users': users,'accounts': accounts, 'account': account, 'personal_rooms': personal_rooms})
 
 @login_required
 def room(request, slug=None):
@@ -32,7 +54,7 @@ def room(request, slug=None):
     except Room.DoesNotExist:
         raise Http404("Room does not exist")
 
-    rooms = Room.objects.exclude(name__startswith='personal_').all()  # Retrieve all rooms excluding 'personal_' rooms
+    rooms = Room.objects.all()  # Retrieve all rooms excluding 'personal_' rooms
     messages = Message.objects.filter(room=room)[:25]
     users = User.objects.exclude(username=request.user.username)
     accounts = Account.objects.exclude(user=request.user).all()
@@ -90,3 +112,9 @@ def create_personal_room(request, username):
         room.save()
 
     return redirect('room', slug=room.slug)
+
+def newchat(request):
+    accounts = Account.objects.exclude(user=request.user).all()
+    account = Account.objects.get(user=request.user)
+    context = {'accounts': accounts, 'account': account}
+    return render(request, 'room/newchat.html', context)
